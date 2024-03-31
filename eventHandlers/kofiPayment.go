0e75f77dea2f5287c3f06ef103fdc3ff562480bc
Dylan Garvis <dylan@garvis.dev>
2024-03-31 15:08:39 -0400

    Added support for kofi payment events.

--- eventHandlers/kofiPayment.go
+++ eventHandlers/kofiPayment.go
@@ -1,1 +1,25 @@
 package eventHandlers
+
+import (
+	"github.com/nats-io/nats.go"
+	"google.golang.org/protobuf/proto"
+	"time"
+	"vip-service/database"
+	"vip-service/github.com/SocialMinecraft/protos/kofi"
+)
+
+// comes in on kofi.payment
+func kofiPayment(db *database.Db, msg *nats.Msg) error {
+	e := kofi.Payment{}
+	if err := proto.Unmarshal(msg.Data, e); err != nil {
+		return err
+	}
+
+	err := db.AddMembership(database.Membership{
+		Email: e.Email,
+		Start: time.Now(),
+		End:   time.Now().AddDate(0, 1, 0),
+	})
+
+	return err
+}
